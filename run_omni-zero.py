4e8c21c169d24a4aa4c6fdfc11da37146508d35b
Gary James Nave (Cognbuild) <59668562+gjnave@users.noreply.github.com>
2024-09-24 05:56:24 -0600

    Update run_omni-zero.py
    Graphical changes

--- run_omni-zero.py
+++ run_omni-zero.py
@@ -168,7 +168,8 @@ def generate(
 
 with gr.Blocks() as demo:
     gr.Markdown("<h1 style='text-align: center'>Omni Zero</h1>")
-    gr.Markdown("<h4 style='text-align: center'>A diffusion pipeline for zero-shot stylized portrait creation [<a href='https://github.com/okaris/omni-zero' target='_blank'>GitHub</a>], [<a href='https://styleof.com/s/remix-yourself' target='_blank'>StyleOf Remix Yourself</a>]</h4>")
+    gr.Markdown("<h4 style='text-align: center'>A diffusion pipeline for zero-shot stylized portrait creation </h4>") 
+    gr.Markdown)"<h4 style='text-align: center'>[<a href='https://open.spotify.com/album/3I2aN4C0clBSM8I1DmoIsN' target='_blank'>Listen to some great music!</a>], [<a href='http://www.patreon.com/cognibuild' target='_blank'>Find more great Installers!</a>]
     with gr.Row():
         with gr.Column():
             with gr.Row():
